99b3199d9c9fd9a5b0adcefd8134bc349055fcda
Hyekyung_Guk <90148517+GukHyekyung@users.noreply.github.com>
2022-05-23 03:29:18 +0900

    fix: BrowserRouter 누락 -> 추가

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {
   CardPage,
   FindPasswordPage,
@@ -7,20 +8,21 @@ import {
   MyPage,
   SocialPage,
 } from './pages';
-import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/join" element={<JoinPage />}></Route>
-        <Route path="/card" element={<CardPage />}></Route>
-        <Route path="/social" element={<SocialPage />}></Route>
-        <Route path="/mypage" element={<MyPage />}></Route>
-        <Route path="/findpassword" element={<FindPasswordPage />}></Route>
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/join" element={<JoinPage />}></Route>
+          <Route path="/card" element={<CardPage />}></Route>
+          <Route path="/social" element={<SocialPage />}></Route>
+          <Route path="/mypage" element={<MyPage />}></Route>
+          <Route path="/findpassword" element={<FindPasswordPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
