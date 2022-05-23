18a564fd08ee8a741beb4823a2375f6ed972c28c
Hyekyung_Guk <90148517+GukHyekyung@users.noreply.github.com>
2022-05-23 22:53:50 +0900

    fix: BrowserRouter를 App.js에서 page/index.js로 이동
    import 'bulma/css/bulma.min.css'; 추가하였음

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,3 +5,19 @@ export { default as LoginPage } from './LoginPage';
 export { default as MainPage } from './MainPage';
 export { default as MyPage } from './MyPage';
 export { default as SocialPage } from './SocialPage';
+
+import 'bulma/css/bulma.min.css';
+
+import App from '../App';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
