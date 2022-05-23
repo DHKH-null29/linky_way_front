be75cd46433a732a4a2eaf4364ec030e52c22b92
pds0309 <ehd0309@gmail.com>
2022-05-23 23:30:26 +0900

    fix: Strict mode 제거

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import 'bulma/css/bulma.min.css';
+
 import App from './App';
-import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 );
