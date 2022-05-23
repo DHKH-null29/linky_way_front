4ca745552f8861540696edc4da812fb71739444e
Hyekyung_Guk <90148517+GukHyekyung@users.noreply.github.com>
2022-05-24 01:13:39 +0900

    fix: box-shadow 사용자 지정에서 styles-shadow 수정
    리뷰 수정요청 완료했습니다!

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,6 +1,6 @@
 import 'bulma/css/bulma.min.css';
 
-import { BorderRadius, Colors } from '../styles';
+import { BorderRadius, Colors, Shadows } from '../styles';
 
 import { Button } from 'react-bulma-components';
 import styled from '@emotion/styled';
@@ -18,7 +18,7 @@ export default Buttons;
 const StyledButton = styled(Button)`
 font-family: 'ImcreSoojin';
 width: 100%;
-box-shadow: 0 5px 4px rgba(0, 0, 0, 0.25);
+box-shadow: ${Shadows.button};
 background-color: ${({ colorType }) => (!colorType ? Colors.mainFirst : Colors.mainSecond)};
 border-radius: ${BorderRadius.button};
 :hover {
