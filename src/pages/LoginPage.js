2b8158c5e8e86f973f6143e90351785334ccd22b
Hyekyung_Guk <90148517+GukHyekyung@users.noreply.github.com>
2022-05-24 16:04:06 +0900

    feat: 동석님이 공유해주신 로그인 폼 추가
    로그인, 회원가입 등 폼 통일을 위해 동석님의 친절 가득한 폼 공유 추가

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,59 @@
+import { Columns, Container, Hero } from 'react-bulma-components';
+
+import AnimatedIcon from '../components/icons/AnimatedIcon';
+import Buttons from '../components/Buttons';
+import IconInput from '../components/IconInput';
+import styled from '@emotion/styled';
+
 const LoginPage = () => {
   return (
     <>
-      <p>로그인 페이지입니다.</p>
+      <Hero size={'fullheight'}>
+        <StyledHeroBody>
+          <Container>
+            <Columns.Column
+              className="is-half-desktop is-offset-3-desktop is-8-tablet is-offset-2-tablet is-fullwidth-mobile"
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <StyledForm>
+                <div className="control">
+                  <IconInput
+                    leftIconComponent={<AnimatedIcon.Email />}
+                    rightIconComponent={<AnimatedIcon.CheckMark />}
+                  />
+                  <p className="is-size-3">&nbsp;</p>
+                </div>
+                <div className="control">
+                  <Buttons>hi</Buttons>
+                </div>
+              </StyledForm>
+            </Columns.Column>
+          </Container>
+        </StyledHeroBody>
+      </Hero>
     </>
   );
 };
+
+const StyledHeroBody = styled(Hero.Body)``;
+
+const StyledForm = styled.form`
+  width: 100%;
+`;
+
+/**
+ * width: 100%;
+  background-color: #fffff9;
+  border-radius: ${BorderRadius.card};
+  border: 2px solid ${Colors.subFirst};
+  box-shadow: ${Shadows.card};
+
+  @media ${Media.desktop} {
+    max-width: 1200px;
+  }
+ */
 
 export default LoginPage;
