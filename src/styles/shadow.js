c9b1486c0cfe60f1692b76aeb69f8a41d285a499
pds0309 <ehd0309@gmail.com>
2022-05-25 14:12:00 +0900

    feat: 인풋 그림자 속성 추가

--- src/styles/shadow.js
+++ src/styles/shadow.js
@@ -4,6 +4,7 @@
 */
 export const Shadows = Object.freeze({
   button: '2px 4px 6px  rgba(0, 0, 0, 0.4)',
+  input: '2px 4px 3px rgba(0,0,0,0.15)',
   card: '1px 5px 4px rgba(0, 0, 0, 0.25)',
   section: '7px 7px 14px rgba(0,0,0,0.18)',
 });
