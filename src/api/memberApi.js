ea71a13634ebeb7f6381330af21f46b23eedc494
pds0309 <ehd0309@gmail.com>
2022-05-28 02:46:20 +0900

    feat: 닉네임 중복 체크 api 추가

--- src/api/memberApi.js
+++ src/api/memberApi.js
@@ -1,9 +1,15 @@
 import { requestForAll } from './config';
 
+const memberApi = 'api/members';
+
 export const onLogin = async loginForm => {
-  return requestForAll.post('/api/members/login', loginForm).then(result => result);
+  return requestForAll.post(memberApi + '/login', loginForm).then(result => result);
 };
 
 export const onJoin = async joinForm => {
-  return requestForAll.post('/api/members', joinForm).then(result => result);
+  return requestForAll.post(memberApi, joinForm).then(result => result);
+};
+
+export const onCheckNicknameDuplication = async nickname => {
+  return requestForAll.get(memberApi + '/nickname', { params: { nickname } });
 };
