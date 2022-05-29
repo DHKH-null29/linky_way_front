212055f3ad0522076bfef06937589e0fe48302cd
Hyekyung_Guk <90148517+GukHyekyung@users.noreply.github.com>
2022-05-30 03:36:54 +0900

    feat: cardPage 레이아웃 추가

--- src/pages/CardPage.js
+++ src/pages/CardPage.js
@@ -1,6 +1,9 @@
+import SearchLayout from '../components/SearchLayout';
+
 const CardPage = () => {
   return (
     <div>
+      <SearchLayout></SearchLayout>
       <p>개인 카드 페이지입니다.</p>
     </div>
   );
