523f2f5f141dc0375bd292f5115dc7f5062d3aef
pds0309 <ehd0309@gmail.com>
2022-06-01 16:39:15 +0900

    feat: 태그 컴포넌트 속성 추가
     수정 버튼 존재 여부 선택 가능, 사이즈 조절 가능

--- src/components/IconTag.js
+++ src/components/IconTag.js
@@ -1,16 +1,24 @@
 import { Icon } from 'react-bulma-components';
 import TagIcon from './icons/TagIcon';
 
-const IconTag = ({ children }) => {
+const IconTag = ({ size, children, writable }) => {
+  const SpanClassName = 'tag is-warning is-rounded is-' + size;
+  const IconClassName = 'is' + size;
   return (
-    <span className="tag is-warning is-medium is-rounded">
-      <Icon>
+    <span className={SpanClassName}>
+      <Icon className={IconClassName}>
         <TagIcon />
+        &nbsp;
       </Icon>
       {children}
-      <button className="delete is-small"></button>
+      {writable && <button className="delete is-small"></button>}
     </span>
   );
+};
+
+IconTag.defaultProps = {
+  size: 'large',
+  writable: false,
 };
 
 export default IconTag;
