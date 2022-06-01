7d129f5cd149aa77b96969050fcf3cafc0910ce5
Hyekyung_Guk <90148517+GukHyekyung@users.noreply.github.com>
2022-06-01 16:32:01 +0900

    fix: 태그 리스트 api 호출로 수정
    태그 리스트 인증 실패로 태그이름이 나오는지 확인하지 못하였다. 수정 예정.

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -1,16 +1,43 @@
 import AnimatedIcon from './icons/AnimatedIcon';
+import { Columns } from 'react-bulma-components';
 import IconTag from '../components/IconTag';
+import { onTagList } from '../api/tagApi';
+import { useState } from 'react';
+
+// import { useNavigate } from 'react-router-dom';
 
 const TagList = () => {
+  const [tags, setTags] = useState([]);
+  //   const navigate = uesNavigate();
+
+  const handleTagListClick = () => {
+    onTagList()
+      .then(response => {
+        setTags(JSON.stringify(response.data));
+        console.log(response);
+      })
+      .catch(error => {
+        setTags(JSON.stringify(error));
+        alert('인증이 필요합니다.');
+        // navigate('/login');
+        // console.log(uesNavigate);
+        console.log(tags);
+      });
+  };
+  console.log(tags);
   return (
     <div>
-      <AnimatedIcon.SkipBack />
-      <IconTag>김아무개</IconTag>
-      <IconTag>이아무개</IconTag>
-      <IconTag>이아무개</IconTag>
-      <IconTag>이아무개</IconTag>
-      <IconTag>이아무개</IconTag>
-      <AnimatedIcon.SkipForward />
+      <Columns>
+        <Columns.Column>
+          <AnimatedIcon.SkipBack onClick={handleTagListClick} />
+          <IconTag>{tags.tagName}</IconTag>
+          <IconTag>이아무개</IconTag>
+          <IconTag>이아무개</IconTag>
+          <IconTag>이아무개</IconTag>
+          <IconTag>이아무개</IconTag>
+          <AnimatedIcon.SkipForward onClick={handleTagListClick} />
+        </Columns.Column>
+      </Columns>
     </div>
   );
 };
