c436d415f761a96c3d08b62fba191096d2fb51d5
pds0309 <ehd0309@gmail.com>
2022-06-02 22:22:22 +0900

    feat: 링크프리뷰 api 추가

---
+++ src/api/linkPreviewApi.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import notFoundImage from '../assets/images/404.JPG';
+
+export const onSelectCardLinkPreview = async url => {
+  return axios({
+    url: `https://rlp-proxy.herokuapp.com/v2?url=${url}`,
+    method: 'get',
+  })
+    .then(response => {
+      const result = response.data.metadata;
+      return { title: result.title, description: result.description, image: result.image };
+    })
+    .catch(error => {
+      if (error.response.status === 404) {
+        return {
+          title: '링크를 찾을 수 없어요!',
+          description: '존재하지 않거나 사라진 링크입니다.',
+          image: notFoundImage,
+        };
+      }
+      console.log(error);
+    });
+};
