e073ac66fc63e572b8740db3b8b633bf5c2cd59b
pds0309 <ehd0309@gmail.com>
2022-06-03 12:43:59 +0900

    feat: 카드 페이지 분류 정보 및 동작 ui 추가

--- src/pages/CardPage.js
+++ src/pages/CardPage.js
@@ -1,21 +1,56 @@
 import { Columns, Hero } from 'react-bulma-components';
+import { FontSize, Media } from '../styles';
+import { Link, useNavigate } from 'react-router-dom';
+import {
+  currentCardClassifier,
+  currentCardState,
+  currentDefaultCardState,
+} from '../state/cardState';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 
 import DownCards from '../components/DownCards';
 import FolderBar from '../components/FolderBar';
 import SearchLayout from '../components/SearchLayout';
-import { currentCardState } from '../state/cardState';
+import { folderHighlightState } from '../state/folderState';
 import { loginState } from '../state/loginState';
+import { onSelectCardsByDefaultMember } from '../api/cardApi';
 import styled from '@emotion/styled';
-import { useNavigate } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
+import useAsync from '../hooks/useAsync';
+import { useEffect } from 'react';
 
 const CardPage = () => {
   const login = useRecoilValue(loginState);
-  const currentCards = useRecoilValue(currentCardState);
+  const [currentCards, setCurrentCards] = useRecoilState(currentCardState);
+  const cardClassifier = useRecoilValue(currentCardClassifier);
+  const [defaultCards, setDefaultCards] = useRecoilState(currentDefaultCardState);
+  const setFolderHighlight = useSetRecoilState(folderHighlightState);
+
   const navigate = useNavigate();
   if (!login) {
     navigate('/login');
   }
+
+  const onLoadCards = async () => {
+    const result = await onSelectCardsByDefaultMember();
+    return result;
+  };
+  const [state, fetch] = useAsync(onLoadCards, [], true);
+
+  useEffect(() => {
+    if (!defaultCards || defaultCards.length === 0) {
+      console.log('fetch default cards');
+      (async () => {
+        const result = await fetch();
+        setDefaultCards(result.data);
+      })();
+    }
+  }, [defaultCards, state]);
+
+  useEffect(() => {
+    setFolderHighlight([]);
+    setCurrentCards(defaultCards);
+  }, [defaultCards]);
+
   return (
     <div>
       <SearchLayout />
@@ -26,6 +61,31 @@ const CardPage = () => {
             <FolderBar></FolderBar>
           </FolderBarWrapper>
           <Columns.Column className="is-8-desktop is-9-tablet is-12-mobile">
+            <Columns className="pl-2 m-3 pb-3">
+              <StyledLink
+                className="mr-5"
+                to={'/card'}
+                onClick={() => {
+                  setCurrentCards(defaultCards);
+                  setFolderHighlight([]);
+                }}
+              >
+                전체보기
+              </StyledLink>
+              <StyledLink to={'/card'} onClick={() => {}}>
+                카드추가+
+              </StyledLink>
+            </Columns>
+            <Columns className="pt-4 pb-1 m-0">
+              <Classifier className="pl-2">
+                &nbsp;[분류] ::&nbsp; <span>전체</span>
+                {' >  ' +
+                  (cardClassifier.classifier ? cardClassifier.classifier + ' > ' : '') +
+                  (cardClassifier.parent ? cardClassifier.parent.name + ' > ' : '') +
+                  cardClassifier.name}
+                <hr />
+              </Classifier>
+            </Columns>
             <Columns className="is-mobile">
               {currentCards.map((value, index) => {
                 return (
@@ -50,5 +110,28 @@ const CardPage = () => {
 };
 
 const FolderBarWrapper = styled(Columns.Column)``;
+
+const Classifier = styled.div`
+  font-size: ${FontSize.huge};
+  @media ${Media.tablet} {
+    font-size: ${FontSize.large};
+  }
+  @media ${Media.mobile} {
+    font-size: ${FontSize.normal};
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: 1px solid underline;
+  text-underline-offset: ${FontSize.normal};
+  opacity: 0.7;
+  font-size: ${FontSize.huge};
+  @media ${Media.tablet} {
+    font-size: ${FontSize.large};
+  }
+  @media ${Media.mobile} {
+    font-size: ${FontSize.normal};
+  }
+`;
 
 export default CardPage;
