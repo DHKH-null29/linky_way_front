d7cf7fb2140ea0abe4d7f9b24976f1ee8e02b58a
Hyekyung_Guk <90148517+GukHyekyung@users.noreply.github.com>
2022-06-03 15:30:47 +0900

    fix: map 함수로 수정

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -1,41 +1,47 @@
 import AnimatedIcon from './icons/AnimatedIcon';
 import { Columns } from 'react-bulma-components';
 import IconTag from '../components/IconTag';
-import { onTagList } from '../api/tagApi';
+import { onGetTagList } from '../api/tagApi';
 import { useState } from 'react';
-
-// import { useNavigate } from 'react-router-dom';
 
 const TagList = () => {
   const [tags, setTags] = useState([]);
-  //   const navigate = uesNavigate();
 
   const handleTagListClick = () => {
-    onTagList()
+    onGetTagList()
       .then(response => {
-        setTags(JSON.stringify(response.data));
+        setTags(response.data);
         console.log(response);
       })
       .catch(error => {
         setTags(JSON.stringify(error));
         alert('인증이 필요합니다.');
-        // navigate('/login');
-        // console.log(uesNavigate);
-        console.log(tags);
       });
   };
-  console.log(tags);
+  console.log(handleTagListClick);
+
+  const TagListItems = () => {
+    onGetTagList().then(response => {
+      console.log(response);
+    });
+  };
+  console.log(TagListItems);
+
   return (
     <div>
-      <Columns>
-        <Columns.Column>
-          <AnimatedIcon.SkipBack onClick={handleTagListClick} />
-          <IconTag>{tags.tagName}</IconTag>
-          <IconTag>이아무개</IconTag>
-          <IconTag>이아무개</IconTag>
-          <IconTag>이아무개</IconTag>
-          <IconTag>이아무개</IconTag>
-          <AnimatedIcon.SkipForward onClick={handleTagListClick} />
+      <Columns className="is-mobile">
+        <Columns.Column className="is-1">
+          <AnimatedIcon.SkipBack onClick={handleTagListClick} size={30} />
+        </Columns.Column>
+        <Columns.Column className="is-8" size>
+          <ul>{TagListItems} </ul>
+
+          {tags.map(value => {
+            return <IconTag key={value.tagId}> {value.tagName} </IconTag>;
+          })}
+        </Columns.Column>
+        <Columns.Column className="is-1">
+          <AnimatedIcon.SkipForward onClick={handleTagListClick} size={30} />
         </Columns.Column>
       </Columns>
     </div>
