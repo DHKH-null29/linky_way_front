375245731a76ef47a26b91d2a02158e474d192d7
pds0309 <ehd0309@gmail.com>
2022-06-03 12:40:39 +0900

    feat: 카드페이지 첫 렌더링 카드 목록 atom, 현재 카드 분류 상태 atom 추가

--- src/state/cardState.js
+++ src/state/cardState.js
@@ -1,11 +1,23 @@
 import { atom } from 'recoil';
+import { persistAtom } from './persist';
 
 export const currentCardState = atom({
   key: 'currentCards',
   default: [],
 });
 
+export const currentDefaultCardState = atom({
+  key: 'currentDefaultCards',
+  default: [],
+  effects_UNSTABLE: [persistAtom],
+});
+
 export const cardChangeState = atom({
   key: 'cardChange',
   default: false,
+});
+
+export const currentCardClassifier = atom({
+  key: 'cardClassifier',
+  default: { id: -1, classifier: undefined, name: '', parent: undefined },
 });
