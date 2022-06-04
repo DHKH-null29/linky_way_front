66f978a97ffe1c7ace12ffb4ad87a0ac837c948d
Hyekyung_Guk <90148517+GukHyekyung@users.noreply.github.com>
2022-06-04 17:09:40 +0900

    fix: 아이콘 주석처리 / 태그 리스트 수정
    동석님의 도움으로 페이지 이동시 태그 리스트가 호출 되도록 수정하였음.

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -1,47 +1,50 @@
-import AnimatedIcon from './icons/AnimatedIcon';
+// import AnimatedIcon from './icons/AnimatedIcon';
+
+import { useEffect, useState } from 'react';
+
 import { Columns } from 'react-bulma-components';
 import IconTag from '../components/IconTag';
 import { onGetTagList } from '../api/tagApi';
-import { useState } from 'react';
 
 const TagList = () => {
   const [tags, setTags] = useState([]);
 
-  const handleTagListClick = () => {
+  useEffect(() => {
     onGetTagList()
       .then(response => {
         setTags(response.data);
-        console.log(response);
       })
       .catch(error => {
         setTags(JSON.stringify(error));
         alert('인증이 필요합니다.');
       });
-  };
-  console.log(handleTagListClick);
+  }, []);
 
-  const TagListItems = () => {
-    onGetTagList().then(response => {
-      console.log(response);
-    });
-  };
-  console.log(TagListItems);
+  // const handleTagListClick = () => {
+  //   onGetTagList()
+  //     .then(response => {
+  //       setTags(response.data);
+  //       console.log(response);
+  //     })
+  //     .catch(error => {
+  //       setTags(JSON.stringify(error));
+  //       alert('인증이 필요합니다.');
+  //     });
+  // };
 
   return (
     <div>
       <Columns className="is-mobile">
         <Columns.Column className="is-1">
-          <AnimatedIcon.SkipBack onClick={handleTagListClick} size={30} />
+          {/* <AnimatedIcon.SkipBack onClick={handleTagListClick} size={30} /> */}
         </Columns.Column>
         <Columns.Column className="is-8" size>
-          <ul>{TagListItems} </ul>
-
           {tags.map(value => {
             return <IconTag key={value.tagId}> {value.tagName} </IconTag>;
           })}
         </Columns.Column>
         <Columns.Column className="is-1">
-          <AnimatedIcon.SkipForward onClick={handleTagListClick} size={30} />
+          {/* <AnimatedIcon.SkipForward onClick={handleTagListClick} size={30} /> */}
         </Columns.Column>
       </Columns>
     </div>
