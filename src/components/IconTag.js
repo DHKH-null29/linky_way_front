7f998eacc592a8ee0a9a4d13ae2451bc54c81b94
Hyekyung_Guk <90148517+GukHyekyung@users.noreply.github.com>
2022-06-05 23:01:48 +0900

    fix: 리뷰 반영 스타일 import 삭제

--- src/components/IconTag.js
+++ src/components/IconTag.js
@@ -1,6 +1,5 @@
 import { Icon } from 'react-bulma-components';
 import TagIcon from './icons/TagIcon';
-import styled from '@emotion/styled';
 
 const IconTag = ({ size, children, writable }) => {
   const SpanClassName = 'tag is-warning is-rounded is-' + size;
