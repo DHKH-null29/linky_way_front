a679bcd7dab24847b3982a08bc9c646136fcab4f
pds0309 <ehd0309@gmail.com>
2022-06-07 05:29:23 +0900

    refactor: 모달 구조 리팩터링
    footer 부분을 body 에서 사용할 수 있도록 구성

---
+++ src/components/modals/ModalFooter.js
@@ -0,0 +1,35 @@
+import Buttons from '../Buttons';
+import { Colors } from '../../styles';
+import styled from '@emotion/styled';
+
+const ModalFooter = ({ confirm, onConfirm, onClose, cancel }) => {
+  return (
+    <Footer className="modal-card-foot">
+      <Buttons type="submit" onClick={onConfirm}>
+        {confirm}
+      </Buttons>
+      &nbsp;&nbsp;
+      {cancel && (
+        <Buttons type="button" className="is-light" onClick={onClose}>
+          {cancel}
+        </Buttons>
+      )}
+    </Footer>
+  );
+};
+
+ModalFooter.defaultProps = {
+  confirm: '확인',
+  cancel: '닫기',
+};
+
+const Footer = styled.footer`
+  button {
+    font-size: 0.95rem;
+  }
+
+  background-color: white;
+  border-top: 1px solid ${Colors.mainFirst};
+`;
+
+export default ModalFooter;
