d43b04988973675a00718e31abddadd6ce500b14
Hyekyung_Guk <90148517+GukHyekyung@users.noreply.github.com>
2022-06-07 20:30:04 +0900

    fix: 태그 삭제 기능 수정

--- src/components/IconTag.js
+++ src/components/IconTag.js
@@ -1,7 +1,40 @@
+import { Colors } from '../styles';
 import { Icon } from 'react-bulma-components';
+import Swal from 'sweetalert2';
 import TagIcon from './icons/TagIcon';
+import { currentTagState } from '../state/tagState';
+import { onDeleteTag } from '../api/tagApi';
+import { useRecoilState } from 'recoil';
 
-const IconTag = ({ size, children, writable }) => {
+const IconTag = ({ size, tagId, index, children, writable }) => {
+  const [currentTags, setCurrentTags] = useRecoilState(currentTagState);
+
+  const handleTagDeleteButtonClick = async () => {
+    Swal.fire({
+      icon: 'question',
+      text: '정말 태그를 삭제하실 건가요?',
+      showCancelButton: true,
+      confirmButtonColor: Colors.successFirst,
+      cancelButtonColor: Colors.warningFirst,
+      confirmButtonText: '네',
+      cancelButtonText: '아니요',
+    }).then(async result => {
+      if (result.isConfirmed) {
+        onDeleteTag(tagId)
+          .then(() => {
+            const newTags = [...currentTags];
+            newTags.splice(index, 1);
+            setCurrentTags(newTags);
+          })
+          .catch(error => {
+            Swal.fire({
+              icon: 'error',
+              text: error.details,
+            });
+          });
+      }
+    });
+  };
   const SpanClassName = 'tag is-warning is-rounded is-' + size;
   const IconClassName = 'is' + size;
   return (
@@ -11,7 +44,9 @@ const IconTag = ({ size, children, writable }) => {
         &nbsp;
       </Icon>
       {children}
-      {writable && <button className="delete is-small"></button>}
+      {writable && (
+        <button className="delete is-small" onClick={handleTagDeleteButtonClick}></button>
+      )}
     </span>
   );
 };
--- src/components/TagList.js
+++ src/components/TagList.js
@@ -6,58 +6,22 @@ import { Columns } from 'react-bulma-components';
 import IconTag from '../components/IconTag';
 import { onGetTagList } from '../api/tagApi';
 
-// import onDeleteTagList from '../api/tagApi'
-
-// import { useRecoilState, useSetRecoilState } from 'recoil';
-
-// import { Colors } from '../styles';
-
-// import Swal from 'sweetalert2';
-
 const TagList = () => {
   const [tags, setTags] = useState([]);
-  // const [currentTags, setCurrentTags] = useRecoilState(currentTagState);
-  // const setCardChange = useSetRecoilState(cardChangeState);
 
   useEffect(() => {
-    onGetTagList()
-      .then(response => {
-        setTags(response.data);
-      })
-      .catch(error => {
-        setTags(JSON.stringify(error));
-        alert('인증이 필요합니다.');
-      });
-  }, []);
+    if (tags || tags.length === 0) {
+      onGetTagList()
+        .then(response => {
+          setTags(response.data);
+        })
+        .catch(error => {
+          setTags(JSON.stringify(error));
+          alert('인증이 필요합니다.');
+        });
+    }
+  }, [tags]);
 
-  // const handleTagDeleteClick = async () => {
-  //   Swal.fire({
-  //     icon: 'question',
-  //     text: '정말 태그를 삭제하실 건가요?',
-  //     showCancelButton: true,
-  //     confirmButtonColor: `${Colors.successFirst}`,
-  //     cancelButtonColor: `${Colors.warningFirst}`,
-  //     confirmButtonText: 'Yes, delete it!',
-  //     cancelButtonText: 'No, cancel!',
-  //     reverseButtons: true,
-  //   }).then(async result => {
-  //     if (result.isConfirmed) {
-  //       onDeleteTagList(id)
-  //         .then(() => {
-  //           const newCards = [...currentTags];
-  //           newCards.splice(index, 1);
-  //           setCurrentTags(newTags);
-  //           setTagChange(true);
-  //         })
-  //         .catch(error => {
-  //           Swal.fire({
-  //             icon: 'error',
-  //             text: error.details,
-  //           });
-  //         });
-  //     }
-  //   });
-  // };
   // const handleTagListClick = () => {
   //   onGetTagList()
   //     .then(response => {
@@ -77,9 +41,9 @@ const TagList = () => {
           {/* <AnimatedIcon.SkipBack onClick={handleTagListClick} size={30} /> */}
         </Columns.Column>
         <Columns.Column className="is-8" size>
-          {tags.map(value => {
+          {tags.map((value, index) => {
             return (
-              <IconTag writable={true} key={value.tagId}>
+              <IconTag writable={true} key={value.tagId} tagId={value.tagId} index={index}>
                 {value.tagName}
               </IconTag>
             );
