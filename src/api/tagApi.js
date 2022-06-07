2fdf4627335e970231fd2522ccb641be14c69c79
Hyekyung_Guk <90148517+GukHyekyung@users.noreply.github.com>
2022-06-07 13:25:35 +0900

    feat: 태그 삭제 api 추가

--- src/api/tagApi.js
+++ src/api/tagApi.js
@@ -4,4 +4,7 @@ const tagApi = 'api/tags';
 
 export const onGetTagList = async () => {
   return requestForAuth.get(tagApi + '/table');
-};
+
+export const onDeleteTagList = async tagId => {
+  return requestForAuth.delete(tagApi + '/${tagId}');
+}
