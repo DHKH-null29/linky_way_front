a622cdb5940b374992420750daeba94bb3c3efd0
pds0309 <ehd0309@gmail.com>
2022-06-14 12:36:52 +0900

    fix: 폴더 목록 조회 api 함수가 기본적으로 재귀 형태로 응답하게 변경

--- src/api/folderApi.js
+++ src/api/folderApi.js
@@ -1,9 +1,25 @@
+import { FOLDER } from '../constants/business';
 import { requestForAuth } from './config';
 
 const folderApi = 'api/folders';
 
-export const onSelectFolderList = () => {
-  return requestForAuth.get(folderApi + '/super');
+const setFolderList = folderList => {
+  return folderList.map(folder => flatChildFolderList(folder)).flat(FOLDER.DEPTH_LIMIT + 1);
+};
+
+const flatChildFolderList = currentFolder => {
+  if (!currentFolder.childFolderList) {
+    return currentFolder;
+  }
+  return [currentFolder, currentFolder.childFolderList.map(child => flatChildFolderList(child))];
+};
+
+export const onSelectFolderList = async () => {
+  console.log('CALL FOLDER LIST');
+  return requestForAuth
+    .get(folderApi + '/super')
+    .then(response => setFolderList(response.data))
+    .catch(() => []);
 };
 
 export const onDeleteFolder = folderId => {
