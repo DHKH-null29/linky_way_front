39dbf566baabefe28a013b4cb68c778df5220dfd
Hyekyung_Guk <90148517+GukHyekyung@users.noreply.github.com>
2022-06-16 16:22:06 +0900

    feat: 패키지 리스트 추가

--- src/pages/SocialPage.js
+++ src/pages/SocialPage.js
@@ -1,13 +1,54 @@
+import { Columns, Hero } from 'react-bulma-components';
+
 import Layout from '../components/socialPages/Layout';
 import Package from '../components/socialPages/Package';
+import { currentPackageState } from '../state/socialState';
+import { onGetPackageList } from '../api/socialApi';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
 
 const SocialPage = () => {
+  const [packages, setPackages] = useRecoilState(currentPackageState);
+
+  useEffect(
+    tagId => {
+      if (!packages || packages.length === 0) {
+        onGetPackageList(tagId)
+          .then(response => {
+            setPackages(response.data);
+          })
+          .catch(error => {
+            setPackages(JSON.stringify(error));
+            alert('인증이 필요합니다.');
+          });
+      }
+    },
+    [packages],
+  );
+
   return (
-    <>
-      <Layout></Layout>
-      <Package></Package>
-      <p>소셜 페이지입니다.</p>
-    </>
+    <div>
+      <Layout />
+      <Hero className="medium">
+        <Hero.Body className="columns">
+          <Columns.Column>
+            <Columns className="is-mobile">
+              {packages.map((value, index) => {
+                return (
+                  <Columns.Column key={index} className="is-3-desktop  is-6-tablet is-half-mobile">
+                    <Package
+                      id={value.memeberId}
+                      nickname={value.nickname}
+                      numberOfCard={value.numberOfCard}
+                    />
+                  </Columns.Column>
+                );
+              })}
+            </Columns>
+          </Columns.Column>
+        </Hero.Body>
+      </Hero>
+    </div>
   );
 };
 
