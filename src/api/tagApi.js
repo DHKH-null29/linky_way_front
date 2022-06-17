eb3f5339c8feb821439fae174ca3605a012906b8
pds0309 <ehd0309@gmail.com>
2022-06-17 10:36:53 +0900

    feat: 태그 추가 api 함수 추가

--- src/api/tagApi.js
+++ src/api/tagApi.js
@@ -6,6 +6,10 @@ export const onGetTagList = () => {
   return requestForAuth.get(tagApi + '/table');
 };
 
+export const onAddTag = addTagForm => {
+  return requestForAuth.post(tagApi, addTagForm);
+};
+
 export const onDeleteTag = tagId => {
   return requestForAuth.delete(tagApi + `/${tagId}`);
 };
