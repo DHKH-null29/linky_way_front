35c673983f601151ddd120eab26956f51fb0d24e
Hyekyung_Guk <90148517+GukHyekyung@users.noreply.github.com>
2022-06-23 16:02:41 +0900

    fix: 비밀번호 수정 인풋 추가 및 이메일 폼 위치 변경

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -20,6 +20,8 @@ const MyPage = () => {
   const initialValues = {
     email: '',
     nickname: '',
+    password: '',
+    checkPassword: '',
   };
 
   const validationSchema = Yup.object().shape({
@@ -31,6 +33,15 @@ const MyPage = () => {
       .strict(true)
       .required('닉네임을 입력해주세요')
       .matches(/^[a-zA-Z0-9가-힣_]{2,10}$/, '2~10 글자의 문자를 입력해주세요'),
+    password: Yup.string()
+      .required('비밀번호를 입력하세요')
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{4,16}$/,
+        '비밀번호는 4~16자의 대소영문자,숫자,특수문자를 포함해야 합니다',
+      ),
+    checkPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], '비밀번호가 일치하지 않습니다.')
+      .required('확인 비밀번호를 입력해주세요.'),
   });
 
   const { errors, handleBlur, handleSubmit, handleChange, touched, values } = useFormik({
@@ -134,29 +145,6 @@ const MyPage = () => {
               <StyledForm onSubmit={handleSubmit}>
                 <p className="container has-text-centered title is-2">My Page</p>
                 <DevideLine space="small" color="none" />
-                <label className="label">이메일</label>
-                <IconInput
-                  name="email"
-                  type="text"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.email}
-                  autocomplete="off"
-                  placeholder="이메일을 입력하세요"
-                  required
-                  leftIconComponent={<AnimatedIcon.Email />}
-                  rightIconComponent={resolveRightIconComponent('email')}
-                />
-                <p
-                  className="m-2"
-                  style={{ color: errors.email ? Colors.warningFirst : Colors.successFirst }}
-                >
-                  &nbsp;{touched.email && (errors.email || '이메일 입력이 확인되었어요')}
-                </p>
-                <Buttons type="button" onClick={handleEmailAuthenticationButtonClick}>
-                  이메일 인증번호 전송
-                </Buttons>
-                <DevideLine space="medium" color="none" />
                 <Columns>
                   <Columns.Column className="is-12 pb-0" style={{ width: '100%' }}>
                     <label className="label">닉네임</label>
@@ -190,8 +178,75 @@ const MyPage = () => {
                         (validNickname ? '닉네임 검증 완료!' : '닉네임 입력이 확인되었어요'))}
                   </p>
                 </Columns>
-                <DevideLine space="medium" color="none" />
+                <p className="is-size-6">&nbsp;</p>
+                <label className="label">비밀번호</label>
+                <IconInput
+                  id="password"
+                  name="password"
+                  type="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                  autocomplete="off"
+                  placeholder="비밀번호를 입력하세요"
+                  required
+                  leftIconComponent={<AnimatedIcon.Password />}
+                  rightIconComponent={resolveRightIconComponent('password')}
+                />
+                <p
+                  className="m-2"
+                  style={{ color: errors.password ? Colors.warningFirst : Colors.successFirst }}
+                >
+                  &nbsp;{touched.password && (errors.password || '비밀번호 입력이 확인되었어요')}
+                </p>
+                <label className="label">비밀번호 확인</label>
+                <IconInput
+                  name="checkPassword"
+                  type="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.checkPassword}
+                  autocomplete="off"
+                  placeholder="비밀번호를 확인해줏세요"
+                  required
+                  leftIconComponent={<AnimatedIcon.Password />}
+                  rightIconComponent={resolveRightIconComponent('checkPassword')}
+                />
+                <p
+                  className="m-2"
+                  style={{
+                    color: errors.checkPassword ? Colors.warningFirst : Colors.successFirst,
+                  }}
+                >
+                  &nbsp;
+                  {touched.checkPassword &&
+                    (errors.checkPassword || '비밀번호 입력이 확인되었어요')}
+                </p>
+                <p className="is-size-4">&nbsp;</p>
                 <Buttons type={'submit'}>수정하기</Buttons>
+                <DevideLine space="medium" color="none" />
+                <label className="label">이메일</label>
+                <IconInput
+                  name="email"
+                  type="text"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                  autocomplete="off"
+                  placeholder="이메일을 입력하세요"
+                  required
+                  leftIconComponent={<AnimatedIcon.Email />}
+                  rightIconComponent={resolveRightIconComponent('email')}
+                />
+                <p
+                  className="m-2"
+                  style={{ color: errors.email ? Colors.warningFirst : Colors.successFirst }}
+                >
+                  &nbsp;{touched.email && (errors.email || '이메일 입력이 확인되었어요')}
+                </p>
+                <Buttons type="button" onClick={handleEmailAuthenticationButtonClick}>
+                  이메일 인증번호 전송
+                </Buttons>
                 <p className="is-size-3">&nbsp;</p>
                 <DevideLine space="medium" color="none" />
                 <label className="label">회원 탈퇴하고 싶어요</label>
