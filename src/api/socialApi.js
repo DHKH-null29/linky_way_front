ce6aaa951759b77593ea0e2046351e391e754bf6
Hyekyung_Guk <90148517+GukHyekyung@users.noreply.github.com>
2022-06-24 20:47:38 +0900

    feat: 패키지 api 추가

--- src/api/socialApi.js
+++ src/api/socialApi.js
@@ -1,7 +1,19 @@
 import { requestForAuth } from './config';
 
-const packageApi = 'api/card/package';
+const packageApi = 'api/cards/package';
+const searchByPackageApi = 'api/search/social';
 
+// 태그이름 검색 패키지 조회 api
+export const onGetSearchByPackage = async tagName => {
+  return requestForAuth.get(searchByPackageApi + `/${tagName}`);
+};
+
+// 패키지 내부의 카드 조회 api
 export const onGetPackageByCardList = async tagId => {
   return requestForAuth.get(packageApi + `/${tagId}`);
+};
+
+// 패키지 카드 복사 api
+export const onCopyPackageByCard = async () => {
+  return requestForAuth.post(packageApi + `/copy`);
 };
