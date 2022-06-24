b3c573b2e0de27aa324c9cafd51c4f7beda8847e
Hyekyung_Guk <90148517+GukHyekyung@users.noreply.github.com>
2022-06-24 20:50:32 +0900

    feat: 소셜 검색 기능 추가
    api 데이터는 가져오지만.. 패키지에 값을 옮기지 못하였다.

--- src/pages/SocialPage.js
+++ src/pages/SocialPage.js
@@ -1,18 +1,22 @@
 import { Columns, Hero } from 'react-bulma-components';
+import { useEffect, useState } from 'react';
 
+import Package from '../components/social/Package';
 import SocialLayout from '../components/social/SocialLayout';
 
-// import Package from '../components/socialPages/Package';
+// import { onGetSearchByPackage } from '../api/socialApi';
 
 // import { currentPackageState } from '../state/socialState';
-// import { onGetPackageByCardList } from '../api/socialApi';
-// import { useEffect } from 'react';
+
 // import { useRecoilState } from 'recoil';
 
-const SocialPage = () => {
-  // const [packages, setPackages] = useRecoilState(currentPackageState);
+// import { onGetPackageByCardList } from '.././api/socialApi';
 
-  // 패키지 클릭시 카드 목록 보여주기 api
+const SocialPage = () => {
+  const [packages, setPackages] = useState([]);
+  const [query, setQuery] = useState('');
+
+  // 패키지 내부의 카드 조회 api
   // const handlePackageClick = () => {
   //   onGetPackageByCardList(tagId)
   //     .then(response => {
@@ -21,19 +25,42 @@ const SocialPage = () => {
   //     })
   // };
 
-  // 패키지 목록 보여주는 api 사용하기
-  // useEffect(
-  //   tagId => {
-  //     if (!packages || packages.length === 0) {
-  //       onGetPackageList(tagId)
-  //         .then(response => {
-  //           setPackages(response.data);
-  //         })
-  //     }
-  //   },
-  //   [packages],
-  // );
+  // 태그이름 검색 패키지 조회 api
+  // useEffect(tagName => {
+  //   if (!packages || packages.length === 0) {
+  //     onGetSearchByPackage(tagName).then(response => {
+  //       setPackages(response.data);
+  //       console.log(response.data);
+  //     });
+  //   }
+  // }, []);
 
+  useEffect(() => {
+    if (query.length > 0) {
+      socialSearchByPackageHandler(query, true);
+      console.log(onGetSearchByPackage);
+    }
+  }, [query]);
+
+  const socialSearchByPackageHandler = async (query, reset) => {
+    const params = {
+      query: query,
+      size: 10,
+    };
+
+    const { data } = await onGetSearchByPackage(params);
+    if (reset) {
+      setPackages(data.documents);
+    } else {
+      setPackages(packages.concat(data.documents));
+    }
+  };
+
+  const onGetSearchByPackage = text => {
+    setQuery(text);
+  };
+
+  // console.log(onGetSearchByPackage);
   return (
     <div>
       <SocialLayout />
@@ -41,17 +68,21 @@ const SocialPage = () => {
         <Hero.Body className="columns">
           <Columns.Column>
             <Columns className="is-mobile">
-              {/* {packages.map((value, index) => {
-                return (
-                  <Columns.Column key={index} className="is-3-desktop  is-6-tablet is-half-mobile">
-                    <Package
-                      id={value.memeberId}
-                      nickname={value.nickname}
-                      numberOfCard={value.numberOfCard}
-                    />
-                  </Columns.Column>
-                );
-              })} */}
+              {packages &&
+                packages.map((value, index) => {
+                  return (
+                    <Columns.Column
+                      key={index}
+                      className="is-3-desktop  is-6-tablet is-half-mobile"
+                    >
+                      <Package
+                        id={value.memeberId}
+                        nickname={value.nickname}
+                        numberOfCard={value.numberOfCard}
+                      />
+                    </Columns.Column>
+                  );
+                })}
             </Columns>
           </Columns.Column>
         </Hero.Body>
