6011cdc3a9978198555b15297d36cd6485a2bf0a
pds0309 <ehd0309@gmail.com>
2022-06-27 07:27:34 +0900

    fix: 모달 사이즈 수정

--- src/components/modals/Modals.js
+++ src/components/modals/Modals.js
@@ -1,4 +1,5 @@
-import { Colors } from '../../styles';
+import { Colors, Media } from '../../styles';
+
 import { Modal } from 'react-bulma-components';
 import styled from '@emotion/styled';
 import useClickAway from '../../hooks/useClickAway';
@@ -10,19 +11,26 @@ const Modals = ({ children, title, active, onClose, ...props }) => {
   return (
     <StyledModal show={active} {...props}>
       <div className="modal-background" style={{ background: 'none' }} onClick={onClose} />
-      <div ref={ref} className="modal-card">
+      <StyledCard ref={ref} className="modal-card">
         <header className="modal-card-head" style={{ backgroundColor: Colors.layout }}>
           <p className="modal-card-title">{title || ''}</p>
           <button onClick={onClose} className="delete" aria-label="close" />
         </header>
         <section className="modal-card-body">{children}</section>
-      </div>
+      </StyledCard>
     </StyledModal>
   );
 };
 
 const StyledModal = styled(Modal)`
   font-family: 'ImcreSoojin';
+`;
+
+const StyledCard = styled.div`
+  @media ${Media.mobile} {
+    max-width: 90%;
+    margin-right: 10%;
+  }
 `;
 
 export default Modals;
