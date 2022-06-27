7ae7b9f4beaeecd34915f2a41c506fd84f12567d
pds0309 <ehd0309@gmail.com>
2022-06-28 04:26:20 +0900

    feat: 소셜 api 추가

--- src/api/socialApi.js
+++ src/api/socialApi.js
@@ -4,13 +4,15 @@ const packageApi = 'api/cards/package';
 const searchByPackageApi = 'api/search/social';
 
 // 태그이름 검색 패키지 조회 api
-export const onGetSearchByPackage = async tagName => {
-  return requestForAuth.get(searchByPackageApi + `/${tagName}`);
+export const onSearchPackageByTagName = async (tagName, isLike = true, page = 0, size = 24) => {
+  return requestForAuth
+    .get(searchByPackageApi + `?isLike=${isLike}&tagName=${tagName}&page=${page}&size=${size}`)
+    .then(response => response.data);
 };
 
 // 패키지 내부의 카드 조회 api
-export const onGetPackageByCardList = async tagId => {
-  return requestForAuth.get(packageApi + `/${tagId}`);
+export const onSearchPackageByCardList = async tagId => {
+  return requestForAuth.get(packageApi + `/${tagId}?size=999`);
 };
 
 // 패키지 카드 복사 api
