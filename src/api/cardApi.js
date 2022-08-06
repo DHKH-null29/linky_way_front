f844ae9d18c24389fefef23982db0d911ba38ec8
pds0309 <ehd0309@gmail.com>
2022-08-06 21:30:40 +0900

    feat: card 조회 api 함수 페이징 관련 변경 반영

--- src/api/cardApi.js
+++ src/api/cardApi.js
@@ -1,40 +1,51 @@
+import { CARD } from '../constants/business';
 import { requestForAuth } from './config';
 
 const cardApi = 'api/cards';
 
-export const onSelectCardsByDefaultMember = () => {
-  return requestForAuth.get(cardApi + '/all');
+function getLastIndexParam(lastIdx) {
+  return lastIdx ? '&lastIdx=' + lastIdx : '';
+}
+
+export const onSelectCardsByDefaultMember = (lastIdx, size = CARD.GLOBAL_PAGING_SIZE) => {
+  const lastIndexParam = getLastIndexParam(lastIdx);
+  return requestForAuth.get(cardApi + `/all?size=${size + lastIndexParam}`);
 };
 
-export const onSelectCardsByFolder = async (folderId, findDeep = false) => {
-  return requestForAuth
-    .get(cardApi + `/folder/${folderId}`, { params: { findDeep } })
-    .then(response => {
-      return response;
-    })
-    .catch(() => {
-      return { data: [] };
-    });
+export const onSelectCardsByFolder = async (
+  folderId,
+  findDeep = false,
+  lastIdx,
+  size = CARD.GLOBAL_PAGING_SIZE,
+) => {
+  const lastIndexParam = getLastIndexParam(lastIdx);
+  return requestForAuth.get(
+    cardApi + `/folder/${folderId}?findDeep=${findDeep}&size=${size + lastIndexParam}`,
+  );
+};
+
+export const onSelectCardsByTagId = (tagId, lastIdx, size = CARD.GLOBAL_PAGING_SIZE) => {
+  const lastIndexParam = getLastIndexParam(lastIdx);
+  return requestForAuth.get(cardApi + `/tag/${tagId}?size=${size + lastIndexParam}`);
+};
+
+export const onSelectCardsByKeyword = (keyword, lastIdx, size = CARD.GLOBAL_PAGING_SIZE) => {
+  const lastIndexParam = getLastIndexParam(lastIdx);
+  return requestForAuth.get(
+    cardApi + `/personal/keyword?keyword=${keyword}&size=${size + lastIndexParam}`,
+  );
+};
+
+export const onSelectCardById = async cardId => {
+  return requestForAuth.get(cardApi + `/${cardId}`).then(response => response.data);
 };
 
 export const onDeleteCard = cardId => {
   return requestForAuth.delete(cardApi + `/${cardId}`);
 };
 
-export const onSelectCardsByTagId = tagId => {
-  return requestForAuth.get(cardApi + `/tag/${tagId}`);
-};
-
 export const onAddCard = cardRequest => {
   return requestForAuth.post(cardApi, cardRequest);
-};
-
-export const onSelectCardsByKeyword = keyword => {
-  return requestForAuth.get(cardApi + '/personal/keyword', { params: { keyword } });
-};
-
-export const onSelectCardById = async cardId => {
-  return requestForAuth.get(cardApi + `/${cardId}`).then(response => response.data);
 };
 
 export const onUpdateCardById = async (cardId, cardRequest) => {
