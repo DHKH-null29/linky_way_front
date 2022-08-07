932d32442faccabbf68a8608c6a1bc4d5102ef40
pds0309 <ehd0309@gmail.com>
2022-08-07 12:37:57 +0900

    feat: 낙관적 업데이트 반영용 card 포맷에 날짜 데이터 추가

--- src/utils/cardUtils.js
+++ src/utils/cardUtils.js
@@ -1,9 +1,12 @@
 export const makeCardFromRequest = (cardId, { body }) => {
-  const newCard = (({ title, content, isPublic, link }) => ({
+  const newCard = (({ title, content, isPublic, link, createdAt, modifiedAt, deletedAt }) => ({
     title,
     content,
     isPublic,
     link,
+    createdAt,
+    modifiedAt,
+    deletedAt,
   }))(body);
   newCard.tags = body.tagIdSet.map(id => (id = { tagId: id }));
   newCard.cardId = cardId;
